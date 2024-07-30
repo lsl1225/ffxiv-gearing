506e088d885c0a0be5159f2675d3e2270e7f2061
lsl1225 <lsl1225@users.noreply.github.com>
2024-07-30 07:36:43 +0000

    Deploying to gh-pages from @ lsl1225/ffxiv-gearing@07a40c4620d1a36393ed967ad14d045be68edd1b 🚀

--- data/out/patches.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  data: '7.01',
-  next: '7.0',
-  current: '6.99',
-};
--- src/views/components/contexts.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { IStore } from '../../stores';
-
-export const StoreContext = React.createContext<IStore>(undefined as any);
-
-export function useStore(): IStore {
-  return React.useContext(StoreContext);
-}
